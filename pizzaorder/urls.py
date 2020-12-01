21aab1b239fa7269a3f46a941f9dcadb8db65d5f
Barış Ortaç <baris.ortac@hamurlabs.com>
2020-12-02 00:06:59 +0300

    clean up unused(for now) imports(for swagger) in urls.py

--- pizzaorder/urls.py
+++ pizzaorder/urls.py
@@ -1,13 +1,5 @@
-from django.conf import settings
-from django.urls import path, re_path, include, reverse_lazy
-from django.conf.urls.static import static
-from django.contrib import admin
-from django.views.generic.base import RedirectView
-from rest_framework.routers import DefaultRouter
 from rest_framework.authtoken import views
-
 from .views import MainView
-from .users.views import UserViewSet, UserCreateViewSet
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
@@ -34,17 +26,13 @@ from django.urls import path, include, re_path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
     path('api/', include('api.urls')),
-
     path('api-token-auth/', views.obtain_auth_token),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-
+    # TODO: ADD SWAGGER
     # re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     # path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     # path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-
-
     # the 'api-root' from django rest-frameworks default router
     # http://www.django-rest-framework.org/api-guide/routers/#defaultrouter
     # re_path(r'^$', RedirectView.as_view(url=reverse_lazy('api-root'), permanent=False)),
